790f54f9461dc0c41a868b960d4e09517864b4d6
Adebola <visenze@olus-MacBook-Pro.local>
2022-10-28 14:15:59 +0100

    added analog clock

--- clock/index.js
+++ clock/index.js
@@ -1,7 +1,12 @@
-const container = document.querySelector('.container');
+const containerDigital = document.querySelector('.container-digital');
+const containerAnalog = document.querySelector('.container-analog');
 const hours = document.querySelector('.hours');
 const mins = document.querySelector('.mins');
 const secs = document.querySelector('.secs');
+const input = document.querySelector('#range');
+const hourHand = document.querySelector('[data-hour-hand]');
+const minuteHand = document.querySelector('[data-minute-hand]');
+const secondHand = document.querySelector('[data-second-hand]');
 
 
 function getTime() {
@@ -11,4 +16,34 @@ function getTime() {
     secs.innerText = date.getSeconds().toString().length === 1 ? '0' + date.getSeconds() : date.getSeconds();
 }
 
+function setAnalogClock() {
+    const currentDate = new Date();
+    const secondsRatio = currentDate.getSeconds() / 60;
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+    setRotation(secondHand, secondsRatio);
+    setRotation(minuteHand, minutesRatio);
+    setRotation(hourHand, hoursRatio)
+
+}
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
 setInterval(() => getTime(), 1000)
+
+input.addEventListener('change', () => {
+    isDigital = input.value === '1'
+    if (isDigital) {
+        containerDigital.classList.remove('hidden');
+        containerAnalog.classList.add('hidden');
+        console.log('yeah')
+        setInterval(() => getTime(), 1000)
+
+    } else {
+        containerDigital.classList.add('hidden');
+        containerAnalog.classList.remove('hidden');
+        setInterval(() => setAnalogClock(), 1000)
+    }
+})
